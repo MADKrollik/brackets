ec43804a797f45ed3377dd0c0f8ac629c9ee0018
Ihor Salfetnyk <4el@ukr.net>
2024-09-08 10:55:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    const openBrackets = [];
+    const closeBrackets = {};
+    const sameBrackets = [];
+
+    bracketsConfig.forEach(([open, close]) => {
+        openBrackets.push(open);
+        closeBrackets[close] = open;
+        if (open === close) {
+            sameBrackets.push(open);
+        }
+    });
+
+    for (let char of str) {
+        if (sameBrackets.includes(char)) {
+            if (stack.length > 0 && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (openBrackets.includes(char)) {
+            stack.push(char);
+        } else if (char in closeBrackets) {
+            if (stack.length === 0 || stack[stack.length - 1] !== closeBrackets[char]) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    
+    return stack.length === 0;
 }
